11b27c4f5990649dc107096885aa59ffbcf3408e
Lucianor01 <lucianorosario02@gmail.com>
2023-05-19 17:51:28 +0200

    Tabella

--- index.php
+++ index.php
@@ -12,77 +12,98 @@ NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es.
 Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
 -->
 
-
+<!-- PHP -->
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+  [
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4
+  ],
+  [
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+  ],
+  [
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+  ],
+  [
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+  ],
+  [
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+  ],
 
-    ];
-
-
+];
 
 ?>
-
-
+<!-- HTML -->
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-<div>
-    <?php
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hotel List</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div class="container">
+    <!-- TABELLA -->
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Parking</th>
+          <th>Vote</th>
+          <th>Distance to Center</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        //? CICLO PER STAMPARE I DATI NELLA TABELLA
         foreach ($hotels as $elem) {
-            echo "<h2>" . $elem['name'] . "</h2>";
-            echo "<p>" . $elem['description'] . "</p>";
-            echo "<p>" . "Parking: " . $elem['parking'] . "</p>";
-            echo "<p>" . "Vote: " . $elem['vote'] . "</p>";
-            echo "<p>" . "Distance to center: " . $elem['distance_to_center'] . "</p>";
+
+          echo "<tr>";
+          echo    "<td>" . $elem['name'] . "</td>";
+          echo    "<td>" . $elem['description'] . "</td>";
+          echo    "<td>" . "Parking: " . ($elem['parking'] ? 'Available' : 'Not available') . "</td>";
+          echo    "<td>" . "Vote: " . $elem['vote'] . "</td>";
+          echo    "<td>" . "Distance to center: " . $elem['distance_to_center'] . ' km' . "</td>";
+          echo "</tr>";
         }
 
-    ?>
-</div>
+        ?>
+      </tbody>
+    </table>
 
+  </div>
+
+  <!-- BOOTSTRAP -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+
 </html>
